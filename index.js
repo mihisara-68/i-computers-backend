4a0f4b9d20047700f45306251deca8cafa84f789
Mihisara-68 <mihisaramadushan68@gmail.com>
2026-04-28 12:58:17 +0530

    fixed ✔

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import express from 'express';
 import mongoose from "mongoose";
-import studentRouter from "./routers/studentRouter.js";
 import userRouter from './routers/userRouter.js';
 import jwt from 'jsonwebtoken';
 import authenticateUser from './middlewares/authentication.js';
